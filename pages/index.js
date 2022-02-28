ec993b9fb7e56249a9beacfdef955a5a0e63a5ca
nokiPro <noki.g2000@gmail.com>
2022-02-28 18:15:13 +0900

    Deploying Your Next.js App

--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>どうも！Webエンジニアを目指して活動している学生です！</p>
-        <p>ご連絡は<a href="https://twitter.com/_nokiPro">twitter</a>まで！</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          どうも！Webエンジニアを目指して活動している学生です！
+          <br />
+          ご連絡は<a href="https://twitter.com/_nokiPro">twitter</a>まで！
         </p>
       </section>
 
